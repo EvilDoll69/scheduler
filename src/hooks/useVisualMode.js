2add9500ddf5f1d6d3e8ef40f487ae01ba5f819a
Tatyana Fediushyn <furer@Tatyanas-MacBook-Pro.local>
2022-01-25 00:58:09 -0500

    Add custom hook useApplicationData

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+
+      setHistory(prev => [...prev]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    } setMode(newMode);
+
+    //   const shortHistory = history.slice(0, -1);
+    //   setHistory([...shortHistory, newMode]);
+    // } else {    
+    // setHistory([...history, newMode]);
+    // }  setMode(newMode);
+  }
+
+  function back() {
+    if (history.length > 1) {
+
+      const shortHistory = history.slice(0, -1);
+      setHistory(shortHistory);
+      setMode(shortHistory[shortHistory.length - 1])
+    }
+  }
+  return { mode, transition, back };
+}
+
