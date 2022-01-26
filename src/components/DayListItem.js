1cfdabad6ceebdb3e4389a9b1e280d34938b2cd9
Tatyana Fediushyn <furer@Tatyanas-MacBook-Pro.local>
2022-01-26 17:18:38 -0500

    Add CYPRESS tests for Navigation

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -16,11 +16,11 @@ export default function DayListItem(props) {
 
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--full": props.spots === 0,
   });
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} key={props.id}>
+    <li className={dayClass} data-testid="day" onClick={() => props.setDay(props.name)} selected={props.selected} key={props.id}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
